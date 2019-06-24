3421e04d6d1262faf601be6ac113fcb945837077
Naman Gupta <01namangupta@gmail.com>
2019-06-25 01:31:23 +0530

    Socket Version Changed and added socket-io-redis adapter

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
+// redisDemo.js
+console.log("asd");
+var redis = require('redis');
+var client = redis.createClient(6379, '13.229.240.75');
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+client.set('my test key', 'my test value', redis.print);
+client.get('my test key', function (error, result) {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    console.log('GET result ->' + result);
+});
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ host: '13.229.240.75', port: 6379 }));
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
